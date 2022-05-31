89bcef3e5dc0fa4fdf4f0e47ca5a23b9735ce94f
JakeD <jakediefenderfer@gmail.com>
2022-05-31 16:24:46 -0400

    add cached elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-const = 
+
+const inp = document.getElementById('inp')
+const addBtn = document.getElementById('submit-button')
+const todoList = document.getElementById('todo-list')
+
+
+addBtn.addEventListener('click', function() {
+  let newItem = todoList.createElement('li').textContent = inp.value
+  inp.value = ''
+  
+})
